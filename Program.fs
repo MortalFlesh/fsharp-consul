4cac9cf4f422d248942a887ca5fef9489fc16f1c
Petr Chromec <mortal_flesh@seznam.cz>
2018-08-02 15:57:09 +0200

    Simplify an example

--- Program.fs
+++ Program.fs
@@ -1,7 +1,31 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open Example.Main
+open System.Text
+open Consul
+
+let uri (url: string) =
+    let uriBuilder = new UriBuilder(url);
+    uriBuilder.Uri
+
+let decode value =
+    match value with
+    | null -> "<null-value>"
+    | value -> Encoding.UTF8.GetString(value, 0, value.Length)
+
+let decodeResultValue (result: QueryResult<KVPair>) =
+    match result.Response with
+    | null -> "<null-response>"
+    | response -> response.Value |> decode
+
+let readValue (consulUrl: string) (key: string) =
+    use client = new ConsulClient()
+    client.Config.Address <- consulUrl |> uri
+
+    printfn "Get value for \"%s\" ..." key
+    let pair = client.KV.Get (key)
+
+    pair.Result |> decodeResultValue
 
 [<EntryPoint>]
 let main argv =
